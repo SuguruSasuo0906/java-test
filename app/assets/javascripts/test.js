69d1a059861e4f359b697701084d37ea488de41b
SuguruSasuo <fingertail0906@gmail.com>
2020-01-19 12:06:34 +0900

    Update test.js

--- app/assets/javascripts/test.js
+++ app/assets/javascripts/test.js
@@ -3,36 +3,36 @@ $(function () {
   // スマホ用サイドメニュー隠し
   $(".contents-box-right").hide();
   while (i < gon.testlists.length) {
-    let index = i;
-    $('#checkbox' + index).hide();
-    $('#test-a-box' + index).hide();
+    let num = i;
+    $('#checkbox' + num).hide();
+    $('#test-a-box' + num).hide();
 
-    $('#' + index).change(function () {
+    $('#' + num).change(function () {
 
       // var cnt = $('input:checkbox:checked').length;
       // console.log(this);
-      // console.log($('#' + index)[0]);
+      // console.log($('#' + num)[0]);
       // console.log(cnt);
       // console.log($(this).parents(".test-a-ul")[0]);
 
       let parent = $(this).parents(".test-a-ul");
       let chk_box = parent.find('input:checkbox:checked').length;
-      let ans_num = gon.testlists[index].test_a_id;
+      let ans_num = gon.testlists[num].test_a_id;
       let select = gon.ansSlct[ans_num - 1].selects;
       // console.log(ans_num);
       // console.log(select);
       // console.log(chk_box);
-      // console.log(index);
+      // console.log(num);
 
       if ($(this).prop('checked')) {
-        $('#checkbox' + index).fadeIn();
+        $('#checkbox' + num).fadeIn();
         if (select <= chk_box) {
-          $('#test-a-box' + gon.testlists[index].test_a_id).fadeIn();
+          $('#test-a-box' + gon.testlists[num].test_a_id).fadeIn();
         }
       } else {
-        $('#checkbox' + index).hide();
+        $('#checkbox' + num).hide();
         if (chk_box == 0) {
-          $('#test-a-box' + gon.testlists[index].test_a_id).hide();
+          $('#test-a-box' + gon.testlists[num].test_a_id).hide();
         }
       };
 
