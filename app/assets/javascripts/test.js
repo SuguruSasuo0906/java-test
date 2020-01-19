33d30a02b246d32f03d7fe955f90f977caa04a3e
SuguruSasuo <fingertail0906@gmail.com>
2020-01-19 12:02:28 +0900

    java

--- app/assets/javascripts/test.js
+++ app/assets/javascripts/test.js
@@ -1,25 +1,39 @@
 $(function () {
   var i = 0;
-
   // スマホ用サイドメニュー隠し
   $(".contents-box-right").hide();
-
   while (i < gon.testlists.length) {
-    const index = i;
+    let index = i;
     $('#checkbox' + index).hide();
-    $('#test-a-wrapper' + index).hide();
+    $('#test-a-box' + index).hide();
 
     $('#' + index).change(function () {
 
-      var cnt = $('input:checkbox:checked').length;
-      console.log(cnt); lllll
+      // var cnt = $('input:checkbox:checked').length;
+      // console.log(this);
+      // console.log($('#' + index)[0]);
+      // console.log(cnt);
+      // console.log($(this).parents(".test-a-ul")[0]);
+
+      let parent = $(this).parents(".test-a-ul");
+      let chk_box = parent.find('input:checkbox:checked').length;
+      let ans_num = gon.testlists[index].test_a_id;
+      let select = gon.ansSlct[ans_num - 1].selects;
+      // console.log(ans_num);
+      // console.log(select);
+      // console.log(chk_box);
+      // console.log(index);
 
       if ($(this).prop('checked')) {
         $('#checkbox' + index).fadeIn();
-        $('#test-a-wrapper' + gon.testlists[index].test_a_id).fadeIn();
+        if (select <= chk_box) {
+          $('#test-a-box' + gon.testlists[index].test_a_id).fadeIn();
+        }
       } else {
         $('#checkbox' + index).hide();
-        $('#test-a-wrapper' + gon.testlists[index].test_a_id).hide();
+        if (chk_box == 0) {
+          $('#test-a-box' + gon.testlists[index].test_a_id).hide();
+        }
       };
 
     });
--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,8 +1,10 @@
 class TestsController < ApplicationController
 
   def index
-    @test_a_lists = TestAList.includes(test_a: :test_q)
+    @test_a_lists = TestAList.all
+    @test_as = TestA.all
     gon.testlists = @test_a_lists
+    gon.ansSlct = @test_as
     @test_qs = TestQ.all
     @test_as = TestA.all
   end
